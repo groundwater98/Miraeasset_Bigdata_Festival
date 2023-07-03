aeec6af522575ab19c0d2e122d0cabfbd7026f5d
MOOK <mukmookk@gmail.com>
2023-07-03 18:12:20 +0900

    #2 [data-engineering] fetch stock data from yfinance

---
+++ DataEngineering/srcs/fetchers/test/test_get_fetcher.py
@@ -0,0 +1,51 @@
+import unittest
+from data_fetcher import DataFetcher
+
+class TestDataFetcher(unittest.TestCase):
+    def setUp(self):
+        self.fetcher = DataFetcher()
+        
+    def test_get_historical_data(self):
+        print("---- Historical Data ----")
+        data = self.fetcher.get_historical_data("AAPL")
+        print(data)
+        self.assertIsNotNone(data)
+        
+    def test_get_actions(self):
+        print("---- Actions ----")
+        data = self.fetcher.get_actions("AAPL")
+        print(data)
+        self.assertIsNotNone(data)
+    
+    def test_get_dividends(self):
+        data = self.fetcher.get_dividends("AAPL")
+        print("---- Dividends ----")
+        print(data)
+        self.assertIsNotNone(data)
+    
+    def test_get_splits(self):
+        data = self.fetcher.get_splits("AAPL")
+        print("---- Splits ----")
+        print(data)
+        self.assertIsNotNone(data)
+        
+    def test_get_capital_gains(self):
+        data = self.fetcher.get_capital_gains("AAPL")
+        print("---- Capital Gain ----")
+        print(data)
+        self.assertIsNotNone(data)
+        
+    def test_get_shares_full(self):
+        data = self.fetcher.get_shares_full("AAPL")
+        print("---- Shares Full ----")
+        print(data)
+        self.assertIsNotNone(data)
+        
+    def test_get_shares_outstanding(self):
+        data = self.fetcher.get_shares_outstanding("AAPL")
+        print("---- Shares Outstanding ----")
+        print(data)
+        self.assertIsNotNone(data)
+    
+if __name__ == '__main__':
+    unittest.main()
