afe0d983a1923d68865069f7d4915ca6f79e625f
mukmookk <mukmookk@gmail.com>
2023-07-19 02:29:34 +0900

    🐛 fix: resolve error in streamHandler in logger class

--- DataEngineering/srcs/helper/logger.py
+++ DataEngineering/srcs/helper/logger.py
@@ -1,25 +1,25 @@
 import logging
+import sys
 
 class Logger:
-    def __init__(self, logger_name, save=False, file_name="app.log", level="INFO"):
+    def __init__(self, logger_name, save=False, file_name="app.log", level="DEBUG"):
         self.logger = logging.getLogger(logger_name)
-        self.set_logger_configuration(save, file_name, level)
+        self._set_logger_configuration(save, file_name, level)
         
-    def set_logger_configuration(self, save, file_name, level):
-        # EX) 2023-07-03 13:37:31,123 - INFO - This is an info message
+    def _set_logger_configuration(self, save, file_name, level):
         formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
-
         level = getattr(logging, level.upper())
+        
+        stream_handler = logging.StreamHandler(sys.stdout)
+        stream_handler.setFormatter(formatter)
+        self.logger.addHandler(stream_handler)
         
         if save:
             file_handler = logging.FileHandler(file_name)
             file_handler.setFormatter(formatter)
             self.logger.addHandler(file_handler)
-        else:
-            stream_handler = logging.StreamHandler()
-            stream_handler.setFormatter(formatter)
-            self.logger.addHandler(stream_handler)
+        
+        self.logger.setLevel(level)
               
     def get_logger(self):
         return self.logger
-        
