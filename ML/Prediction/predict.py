1f4f5b2b7301b7c7676119939e1ae986f5c03b0a
JoEunNam <jen9581@gmail.com>
2023-09-09 20:10:42 +0900

    app.py create for kakao chatbot api

--- ML/Prediction/predict.py
+++ ML/Prediction/predict.py
@@ -2,6 +2,7 @@
 # If the prediction is success, Expand the number of stock
 import math 
 import os
+import pdb
 
 import numpy as np
 import pandas as pd
@@ -33,46 +34,68 @@ def save_stock_plot(rawdata, stock_name="LG_chme"):
 
 
 class windowDataset(Dataset):
-    def __init__(self, y, input_window=80, output_window=20, stride=5):
+    def __init__(self, y, input_window=80, output_window=20, stride=5, n_attr=1):
         #총 데이터의 개수
+        print(y.shape)
+
         L = y.shape[0]
         #stride씩 움직일 때 생기는 총 sample의 개수
         num_samples = (L - input_window - output_window) // stride + 1
 
-        #input과 output
-        X = np.zeros([input_window, num_samples])
-        Y = np.zeros([output_window, num_samples])
+        if n_attr == 1:
+            #input과 output
+            X = np.zeros([input_window, num_samples])
+            Y = np.zeros([output_window, num_samples])
+            for i in np.arange(num_samples):
+                start_x = stride*i
+                end_x = start_x + input_window
+                X[:,i] = y[start_x:end_x]
 
-        for i in np.arange(num_samples):
-            start_x = stride*i
-            end_x = start_x + input_window
-            X[:,i] = y[start_x:end_x]
+                start_y = stride*i + input_window
+                end_y = start_y + output_window
+                Y[:,i] = y[start_y:end_y]
+            X = X.reshape(X.shape[0], X.shape[1], n_attr)
+            Y = Y.reshape(Y.shape[0], Y.shape[1], n_attr)
+            X = X.transpose((1,0,2))
+            Y = Y.transpose((1,0,2))
+            self.x = X
+            self.y = Y
+        else:
+            #input과 output
+            X = np.zeros([input_window, n_attr, num_samples])
+            Y = np.zeros([output_window, n_attr, num_samples])
 
-            start_y = stride*i + input_window
-            end_y = start_y + output_window
-            Y[:,i] = y[start_y:end_y]
+            for i in np.arange(num_samples):
+                start_x = stride*i
+                end_x = start_x + input_window
+                X[:,:,i] = y[start_x:end_x]
 
-        X = X.reshape(X.shape[0], X.shape[1], 1).transpose((1,0,2))
-        Y = Y.reshape(Y.shape[0], Y.shape[1], 1).transpose((1,0,2))
-        self.x = X
-        self.y = Y
-        
+                start_y = stride*i + input_window
+                end_y = start_y + output_window
+                Y[:,:,i] = y[start_y:end_y]
+            print(X.shape)
+            print(Y.shape)
+            X = X.reshape(X.shape[2], X.shape[0], X.shape[1])
+            Y = Y.reshape(Y.shape[2], Y.shape[0], Y.shape[1])
+            self.x = X
+            self.y = Y
         self.len = len(X)
     def __getitem__(self, i):
-        return self.x[i], self.y[i]
+        #return self.x[i], self.y[i]
+        return self.x[i], self.y[i, :-1], self.y[i,1:]
     def __len__(self):
         return self.len
     
 
 class TFModel(nn.Module):
-    def __init__(self,iw, ow, d_model, nhead, nlayers, dropout=0.5):
+    def __init__(self,iw, ow, d_model, nhead, nlayers, dropout=0.5, n_attr=1):
         super(TFModel, self).__init__()
         self.encoder_layer = nn.TransformerEncoderLayer(d_model=d_model, nhead=nhead, dropout=dropout)
         self.transformer_encoder = nn.TransformerEncoder(self.encoder_layer, num_layers=nlayers) 
         self.pos_encoder = PositionalEncoding(d_model, dropout)
 
         self.encoder = nn.Sequential(
-            nn.Linear(1, d_model//2),
+            nn.Linear(n_attr, d_model//2),
             nn.ReLU(),
             nn.Linear(d_model//2, d_model)
         )
@@ -80,7 +103,7 @@ class TFModel(nn.Module):
         self.linear =  nn.Sequential(
             nn.Linear(d_model, d_model//2),
             nn.ReLU(),
-            nn.Linear(d_model//2, 1)
+            nn.Linear(d_model//2, n_attr)
         )
 
         self.linear2 = nn.Sequential(
@@ -102,6 +125,32 @@ class TFModel(nn.Module):
         output = self.linear2(output)
         return output
 
+
+class TFModel2(nn.Module):
+    def __init__(self,d_model, nhead, nhid, nlayers, dropout=0.5, n_attr=7):
+        super(TFModel2, self).__init__()
+        self.transformer = nn.Transformer(d_model=d_model, nhead=nhead, dim_feedforward=nhid, num_encoder_layers=nlayers, num_decoder_layers=nlayers,dropout=dropout)
+        self.pos_encoder = PositionalEncoding(d_model, dropout)
+        self.pos_encoder_d = PositionalEncoding(d_model, dropout)
+        self.linear = nn.Linear(d_model, n_attr)
+        self.encoder = nn.Linear(n_attr, d_model)
+        self.encoder_d = nn.Linear(n_attr, d_model)
+
+    def generate_square_subsequent_mask(self, sz):
+        mask = (torch.triu(torch.ones(sz, sz)) == 1).transpose(0, 1)
+        mask = mask.float().masked_fill(mask == 0, float('-inf')).masked_fill(mask == 1, float(0.0))
+        return mask
+
+    def forward(self, src, tgt, srcmask, tgtmask):
+        src = self.encoder(src)
+        src = self.pos_encoder(src)
+
+        tgt = self.encoder_d(tgt)
+        tgt = self.pos_encoder_d(tgt)
+        output = self.transformer(src.transpose(0,1), tgt.transpose(0,1), srcmask, tgtmask)
+        output = self.linear(output)
+        return output
+    
 
 class PositionalEncoding(nn.Module):
     def __init__(self, d_model, dropout=0.1, max_len=5000):
@@ -126,14 +175,29 @@ def gen_attention_mask(x):
     return mask
 
 
-def evaluate(data_train, device, model):
+def evaluate(data_train, device, model, iw, n_attr, length):
     # 마지막 30*2일 입력으로 넣어서 그 이후 30일 예측 결과 얻음.
-    input = torch.tensor(data_train[-30*3:]).reshape(1,-1,1).to(device).float().to(device)
+    """
+    print(data_train.shape)
+    input = torch.tensor(data_train[-iw:]).reshape(1,-1,n_attr).to(device).float().to(device)
+    print(input.shape)
     model.eval()
     
     src_mask = model.generate_square_subsequent_mask(input.shape[1]).to(device)
     predictions = model(input, src_mask)
     return predictions.detach().cpu().numpy()
+    """
+    input = torch.tensor(data_train[-iw:]).reshape(1,-1,n_attr).to(device).float().to(device)
+    output = torch.tensor(data_train[-1].reshape(1,-1,n_attr)).float().to(device)
+    model.eval()
+    for i in range(length):
+        src_mask = model.generate_square_subsequent_mask(input.shape[1]).to(device)
+        tgt_mask = model.generate_square_subsequent_mask(output.shape[1]).to(device)
+
+        predictions = model(input, output, src_mask, tgt_mask).transpose(0,1)
+        predictions = predictions[:, -1:, :]
+        output = torch.cat([output, predictions.to(device)], axis=1)
+    return torch.squeeze(output, axis=0).detach().cpu().numpy()[1:]
 
 
 def predict(stock, period):
@@ -142,40 +206,74 @@ def predict(stock, period):
           we predict only the stock price of LG Chem, not the stock price \
           of the designated company.\n\n")
     # 이 코드대신 지수형이 spl로 얻어온 data가 rawdata가 되어야 함.
+    # 추가적인 정보 없는건 1729일
+    """
     print(f"Loading Stock Data ...")
+    n_attr = 1
     path = "/".join(base[:-3]+["data","lg_chem_closing_prices.csv"])
     rawdata = pd.read_csv(path)
-
+    
     print(f"Saving Stock data as .png ...")
     save_stock_plot(rawdata, stock)
-
+    #pdb.set_trace()
     print(f"Preprocessing Data with MinMaxScaling ...")
     min_max_scaler = MinMaxScaler()
-    rawdata["Close"] = min_max_scaler.fit_transform(rawdata["Close"].to_numpy().reshape(-1,1))
+    rawdata["Close"] = min_max_scaler.fit_transform(rawdata["Close"].to_numpy().reshape(-1,n_attr))
 
     print(f"Spliting Data ...")
-    train = rawdata[:-60]
+    iw = 30*9
+    ow = 10
+   
+    train = rawdata[:-iw]
     data_train = train["Close"].to_numpy()
-    test = rawdata[-60:]
+    test = rawdata[-iw:]
     data_test = test["Close"].to_numpy()
 
     print(f"Preparing Dataset ...")
-    iw = 30*3
-    ow = 10
-    train_dataset = windowDataset(data_train, input_window=iw, output_window=ow, stride=1)
+    train_dataset = windowDataset(data_train, input_window=iw, output_window=ow, stride=1, n_attr=n_attr)
     train_loader = DataLoader(train_dataset, batch_size=64)
-    #test_dataset = windowDataset(data_test, input_window=iw, output_window=ow, stride=1)
+    #test_dataset = windowDataset(data_test, input_window=iw, output_window=ow, stride=1, n_attr=n_attr)
     #test_loader = DataLoader(test_dataset)
+    """
+    # 성능 올리기위해 종가말고 다른 것도 같이 넣음.
+    # 총 1720일의 data있음
+    print(f"Loading Stock Data ...")
+    n_attr = 7
+    path = "/".join(base[:-3]+["data","lg_chem_prices.csv"])
+    rawdata = pd.read_csv(path)
 
+    #print(f"Saving Stock data as .png ...")
+    #save_stock_plot(rawdata, stock)
+    print(f"Preprocessing Data with MinMaxScaling ...")
+    min_max_scaler = MinMaxScaler()
+    rawdata.loc[:,rawdata.columns] = min_max_scaler.fit_transform(rawdata.to_numpy())
+
+    print(f"Spliting Data ...")
+    iw = 30
+    ow = 10
+    #pdb.set_trace()
+    train = rawdata[:-(iw)]
+    data_train = train.to_numpy()
+    test = rawdata[-(iw):]
+    data_test = test.to_numpy()
+
+    print(f"Preparing Dataset ...")
+    train_dataset = windowDataset(data_train, input_window=iw, output_window=ow, stride=1, n_attr=n_attr)
+    train_loader = DataLoader(train_dataset, batch_size=64)
+    #test_dataset = windowDataset(data_test, input_window=iw, output_window=ow, stride=1, n_attr=n_attr)
+    #test_loader = DataLoader(test_dataset)
+    
     print(f"Model Constructing ...")
     device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
     lr = 1e-4
-    model = TFModel(30*3, 10, 2048, 16, 8, 0).to(device)
+    model = TFModel2(256, 8, 256, 2, 0.1, n_attr).to(device)
+    #model = TFModel2(iw, ow, 512, 8, 4, 0.4, n_attr).to(device)
     criterion = nn.MSELoss()
     optimizer = torch.optim.Adam(model.parameters(), lr=lr)
-
+    
     print("Trainig ...")
-    epoch = 1
+    epoch = 10
+    """
     model.train()
     progress = tqdm(train_loader, total=len(train_loader), leave=True)
     for i in tqdm(range(epoch)):
@@ -189,21 +287,49 @@ def predict(stock, period):
             optimizer.step()
             batchloss += loss
         progress.set_description("loss: {:0.6f}".format(batchloss.cpu().item() / len(train_loader)))
+    """
+    model.train()
+    progress = tqdm(range(epoch))
+    for i in progress:
+        batchloss = 0.0
+        
+        for (inputs, dec_inputs, outputs) in train_loader:
+            optimizer.zero_grad()
+            src_mask = model.generate_square_subsequent_mask(inputs.shape[1]).to(device)
+            tgt_mask = model.generate_square_subsequent_mask(dec_inputs.shape[1]).to(device)
+
+            result = model(inputs.float().to(device), dec_inputs.float().to(device), src_mask, tgt_mask)
+            loss = criterion(result.permute(1,0,2), outputs.float().to(device))
+            
+            loss.backward()
+            optimizer.step()
+            batchloss += loss
+        progress.set_description("{:0.5f}".format(batchloss.cpu().item() / len(train_loader)))
+
 
     print("Predicting ...")
-    # 총 1719일의 data있음
-    
-    result = evaluate(data_test, device, model)
+    result = evaluate(data_test, device, model, iw, n_attr, ow)
+
+    """
     result = min_max_scaler.inverse_transform(result)[0]
     real = rawdata["Close"].to_numpy()
     real = min_max_scaler.inverse_transform(real.reshape(-1,1))[:,0]
-    # print(len(real))
-
+    
+    #pdb.set_trace()
+    """
+    tmp = np.zeros((10,7))
+    tmp[:,:] = result.reshape(10,-1)
+    result = tmp
+    result = min_max_scaler.inverse_transform(result).reshape(-1,10)[3]
+    real = rawdata.to_numpy()
+    real = min_max_scaler.inverse_transform(real)[:,3]
+    
     plt.figure(figsize=(20,5))
-    plt.plot(range(1419,1719),real[1419:], label="real")
+    plt.plot(range(1419,1719),real[1420:], label="real")
+    #plt.plot(range(1419,1719),real[1419:],label="real")
     plt.plot(range(1719-10,1719),result, label="predict")
     plt.legend()
-    path = "/".join(base[:-2]+["models","prediction.jpg"])
+    path = "/".join(base[:-2]+["models","prediction2.jpg"])
     plt.savefig(path)
     print(f"Complete!!")
 
